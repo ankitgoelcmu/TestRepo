53054124bcf49440acc55aadd036794f68eaf5f1
Ankit Goel <ankitgoelcmu@users.noreply.github.com>
2024-05-15 11:43:07 -0700

    Revert "Threat Intel Changes"

--- main.go
+++ main.go
@@ -1,4 +1,2 @@
 this is the file on master this is the file on master this is the file on master this is the file on master this is the file on master this is the file on master 
 this is the file on master this is the file on master this is the file on master this is the file on master this is the file on master this is the file on master 
-below are changes for Threat Intel 
-using new lookup
